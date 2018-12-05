4385c6b62495694b625a2f260ab205e7120e0111
olivier van den broek <olivier.vdbroek@gmail.com>
2018-12-05 15:13:19 +0100

    add map to index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_04_170227) do
+ActiveRecord::Schema.define(version: 2018_12_05_102448) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,8 @@ ActiveRecord::Schema.define(version: 2018_12_04_170227) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
     t.index ["geographical_location_id"], name: "index_business_assets_on_geographical_location_id"
     t.index ["user_id"], name: "index_business_assets_on_user_id"
   end
