fbbbf790dd1ea0dd2196ccbc95eab8eefbedf463
Gautier Delabrousse Mayoux <gautierdlb@gmail.com>
2018-12-07 14:54:13 +0100

    finction format

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
-  def money(price, args = { currency_symbol: true })
-    if :currency_symbol == true
-      number_with_delimiter(price.round, precision: 0, delimiter: " ")
-    else
+  def format_number(price, currency_symbol: true)
+    if currency_symbol
       number_to_currency(price, delimiter: " ", precision: 0, unit: "€")
+    else
+      number_with_delimiter(price.round, precision: 0, delimiter: " ")
     end
   end
 end
