8d1adb73415ac1e2312af878436d43c061fe381e
Gautier Delabrousse Mayoux <gautierdlb@gmail.com>
2018-12-07 13:15:20 +0100

    avec fonction money avec et sans symbole

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
-  def money(price)
-    number_to_currency(price, delimiter: " ", precision: 0, unit: "€")
+  def money(price, args = { currency_symbol: true })
+    if :currency_symbol == true
+      number_with_delimiter(price.round, precision: 0, delimiter: " ")
+    else
+      number_to_currency(price, delimiter: " ", precision: 0, unit: "€")
+    end
   end
 end
