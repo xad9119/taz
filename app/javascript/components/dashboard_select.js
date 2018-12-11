6de27a8162303404699b7ca393f19d62772f87fd
Gautier Delabrousse Mayoux <gautierdlb@gmail.com>
2018-12-11 17:44:41 +0100

    ok

--- app/javascript/components/dashboard_select.js
+++ app/javascript/components/dashboard_select.js
@@ -1,10 +1,7 @@
-console.log('1')
 function initSelect() {
-  console.log('2')
   const init = document.getElementById("option");
   if (init) {
     init.addEventListener('change', (event) => {
-      console.log('3')
       const formulaire = event.currentTarget.closest("form");
       formulaire.submit()
     })}
