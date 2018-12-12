e1f54a4325915eab23d2f94dca34e0d3ad9650a6
Maxime Zarka <maxime.zarka@gmail.com>
2018-12-12 13:20:35 +0100

    graph value per year

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,7 +13,8 @@ import { autocomplete } from '../components/autocomplete';
 import { initSteps } from '../components/form_steps';
 import { initSelect } from '../components/dashboard_select';
 import { xxx } from '../components/checkbox_compare';
-import { chartDfunct } from '../components/chartD';
+import { chart_price_per_asset } from '../components/chart_price_per_asset';
+import { chart_value_per_year } from '../components/chart_value_per_year';
 
 
 if (document.querySelector("#asset_address")){
@@ -27,6 +28,17 @@ if (document.querySelector("#option")){
   initSelect();
 }
 
-chartDfunct();
+if (document.getElementById("price_per_asset")) {
+ const myId = document.getElementById("price_per_asset");
+ const assets = JSON.parse(myId.dataset.assets);
+ const prices = JSON.parse(myId.dataset.prices);
+ chart_price_per_asset(assets, prices);
+};
 
+if (document.getElementById("value_per_year")) {
+ const myId = document.getElementById("value_per_year");
+ const years = JSON.parse(myId.dataset.years);
+ const values = JSON.parse(myId.dataset.values);
+ chart_value_per_year(years, values);
+};
 
